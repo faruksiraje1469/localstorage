b3fc1eda04bcaf276ad7cdfb72cb6bd6f9f8dbb3
faruksiraje1469 <faruksiraje@gmail.com>
2025-03-21 20:44:03 +0600

    first commit

---
+++ cart.js
@@ -0,0 +1,10 @@
+const handleToCart = () => {
+    const productName = document.getElementById('productName');
+    const productQuantity = document.getElementById('productQuantity');
+
+    const name = productName.value;
+    const quantity = productQuantity.value;
+
+
+    console.log(name,quantity)
+}
